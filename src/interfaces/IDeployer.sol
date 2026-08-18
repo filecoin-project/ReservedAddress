d1e3eb34510189fd73e47f32e9afc61e7865f6cf
William Morriss <wjmelements@gmail.com>
2026-08-18 16:22:38 -0500

    wip: working reserve, reveal, deploy, call

--- src/interfaces/IDeployer.sol
+++ src/interfaces/IDeployer.sol
@@ -2,29 +2,32 @@
 pragma solidity ^0.8.36;
 
 interface IDeployer {
-    // permissioned
+    // Permissioned
+    error NotOwner();
     function deploy(address reserved, address initCode) external;
     function call(address deployed, bytes calldata callData) external payable /*returns (raw_bytes)*/;
 
-    // auction
+    // Auction
     function reveal(address reserved, bytes32 salt) external;
     function reserve(address reserved) external;
     function dispute(address reserved) external;
 
     // IERC721
     event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
     function ownerOf(uint256 tokenId) external view returns (address owner);
+    function getApproved(uint256 tokenId) external view returns (address operator);
+    function isApprovedForAll(address owner, address operator) external view returns (bool);
 
     function safeTransferFrom(address from, address to, uint256 tokenId) external;
     function transferFrom(address from, address to, uint256 tokenId) external;
     function approve(address to, uint256 tokenId) external;
     function setApprovalForAll(address operator, bool approved) external;
-    function getApproved(uint256 tokenId) external view returns (address operator);
-    function isApprovedForAll(address owner, address operator) external view returns (bool);
     function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
 
     // IERC721Metadata
     function name() external pure returns (string memory);
     function symbol() external pure returns (string memory);
-    function tokenURI(uint256 tokenId) external view returns (string memory);
+    function tokenURI(uint256 tokenId) external pure returns (string memory);
 }
--- test/Deployment.t.sol
+++ test/Deployment.t.sol
@@ -2,17 +2,61 @@
 pragma solidity ^0.8.36;
 
 import {Test} from "forge-std/Test.sol";
+import {LibClone} from "solady/utils/LibClone.sol";
 
 import {IDeployer} from "../src/interfaces/IDeployer.sol";
+import {Example} from "./Example.sol";
 
 
 contract DeploymentTest is Test {
-    IDeployer internal deployer; 
-
+    // TODO mine a deployer with Nick's Method
+    IDeployer constant DEPLOYER = IDeployer(0x000000000000c57CF0A1f923d44527e703F1ad70);
     function setUp() public {
-        deployer = IDeployer(deployCode("out/Deploy.evm/Deploy.json"));
+        vm.etch(address(DEPLOYER), vm.getDeployedCode("out/Deployer.evm/Deployer.json"));
     }
 
-    function testAuctionDeployCall() public {
+    function testReserveRevealDeployCall() public {
+        bytes32 salt = bytes32(0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef);
+        bytes32 initCodeHash = keccak256(vm.getDeployedCode("out/Init.evm/Init.json"));
+        address reserved = LibClone.predictDeterministicAddress(initCodeHash, salt, address(DEPLOYER));
+
+        address sender = makeAddr("sender");
+        address initCode = makeAddr("initCode");
+        vm.etch(initCode, type(Example).creationCode);
+
+        vm.prank(sender);
+        DEPLOYER.reserve(reserved);
+
+        vm.expectRevert(IDeployer.NotOwner.selector);
+        DEPLOYER.reveal(reserved, salt);
+
+        vm.prank(sender);
+        DEPLOYER.reveal(reserved, salt);
+
+        vm.expectRevert(IDeployer.NotOwner.selector);
+        DEPLOYER.deploy(reserved, initCode);
+
+        vm.prank(sender);
+        DEPLOYER.deploy(reserved, initCode);
+
+        assertEq(reserved.code, type(Example).runtimeCode);
+
+        Example deployed = Example(reserved);
+        assertEq(deployed.owner(), address(DEPLOYER));
+
+        address recipient = makeAddr("recipient");
+
+        vm.expectRevert(IDeployer.NotOwner.selector);
+        DEPLOYER.call{value:1 ether}(reserved, abi.encodeWithSelector(Example.pay.selector, recipient));
+
+        assertEq(recipient.balance, 0);
+
+        vm.deal(sender, 1 ether);
+        vm.prank(sender);
+        DEPLOYER.call{value:1 ether}(reserved, abi.encodeWithSelector(Example.pay.selector, recipient));
+
+        assertEq(recipient.balance, 1 ether);
+        assertEq(reserved.balance, 0);
+        assertEq(sender.balance, 0);
     }
 }
